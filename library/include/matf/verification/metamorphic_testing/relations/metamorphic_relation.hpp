91180e7c1ca692c6c775b6bf0b23210fadd092e3
Đorđe Marić <75815195+djordjemaric@users.noreply.github.com>
2026-09-20 21:05:31 +0200

    feat: capitalization irrelevance wired up (#9)

--- library/include/matf/verification/metamorphic_testing/relations/metamorphic_relation.hpp
+++ library/include/matf/verification/metamorphic_testing/relations/metamorphic_relation.hpp
@@ -9,9 +9,10 @@ namespace matf::verification::metamorphic_testing::relations {
 
 class MetamorphicRelation {
 public:
-    virtual std::string mutate_input(std::string& input) = 0;
+    virtual std::string mutate_input(std::string input) = 0;
     virtual bool holds(std::unordered_set<int>& original, std::unordered_set<int>& modified) = 0;
     virtual ~MetamorphicRelation() = default;
+    constexpr virtual std::string get_name() const = 0;
 
 protected:
     explicit MetamorphicRelation(TokenGenerator& token_generator) : token_generator(token_generator) {};
